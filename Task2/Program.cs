eccb0ed3984c24dff41b5250d524f4803558be08
Николай <Kolya_popov_86@tut.by>
2022-08-13 14:47:42 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает 
+//все целые числа в промежутке от -N до N.4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);         // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue);     // Преобразует строку в целое число
+    return result;                         // Возвращает результат 
+}
+
+int N;
+N = Prompt("Введите число N ");
+for (i = -N; i <= N; i++)
+{
+    Console.WriteLine(i);
+}
